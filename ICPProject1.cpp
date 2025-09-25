f7e664385154fa30c24c9757fe81a6fb9c9691e6
Jakub Sikola <jakub.sikola@tul.cz>
2025-09-25 11:48:15 +0200

    initial commit

---
+++ ICPProject1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+int main() {
+    cv::namedWindow("test_window");
+    cv::Mat mat = cv::Mat::eye(3, 3, CV_8UC1);
+    std::cout << "3x3 Identity Matrix:\n" << mat << std::endl;
+    return 0;
+}
