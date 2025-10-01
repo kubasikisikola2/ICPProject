addb0e1b5196f32dbdad4c5312177bba742bd310
Libor Mahr <libormahr@gmail.com>
2025-10-01 23:19:23 +0200

    Implemented Task 1 point 4.

--- App.cpp
+++ App.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
 
 #include "App.hpp"
 
@@ -44,44 +43,8 @@ int App::run(void)
         cv::Mat frame2;
         frame.copyTo(frame2);
 
-        // convert to grayscale, create threshold, sum white pixels
-        // compute centroid of white pixels (average X,Y coordinate of all white pixels)
-        cv::Point2f center;
-        cv::Point2f center_normalized;
-        int tot = 0;
+        cv::Point2f center_normalized = find_object_luma(frame2);
 
-        for (int y = 0; y < frame.rows; y++) //y
-        {
-            for (int x = 0; x < frame.cols; x++) //x
-            {
-                // load source pixel
-                cv::Vec3b pixel = frame.at<cv::Vec3b>(y, x);
-
-                // compute temp grayscale value (convert from colors to Y)
-                unsigned char Y = 0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0];
-
-                // FIND THRESHOLD (value 0..255)
-                if (Y < 240) {
-                    // set output pixel black
-                    frame2.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 0, 0);
-                }
-                else {
-                    // set output pixel white
-                    frame2.at<cv::Vec3b>(y, x) = cv::Vec3b(255, 255, 255);
-
-                    ++tot;
-                    center.x += (x - center.x) / tot;
-                    center.y += (y - center.y) / tot;
-                    //center_normalized.x += (x / frame.cols - center_normalized.x) / tot;
-                    //center_normalized.y += (y / frame.rows - center_normalized.y) / tot;
-                    center_normalized.x = center.x / frame.cols;
-                    center_normalized.y = center.y / frame.rows;
-                }
-
-            }
-        }
-
-        std::cout << "Center absolute: " << center << '\n';
         std::cout << "Center normalized: " << center_normalized << '\n';
 
         // how long the computation took?
@@ -91,7 +54,6 @@ int App::run(void)
         std::cout << "Elapsed time: " << elapsed_seconds.count() << "sec" << std::endl;
 
         // highlight the center of object
-        draw_cross(frame, center.x, center.y, 25);
         draw_cross_normalized(frame2, center_normalized, 25);
 
         // show me the result
@@ -144,11 +106,51 @@ void App::draw_cross_normalized(cv::Mat& img, cv::Point2f center_normalized, int
 
     cv::Point2f center_absolute(center_normalized.x * img.cols, center_normalized.y * img.rows);
 
-    cv::Point2f p1(center_absolute.x - size / 2, center_absolute.y);
-    cv::Point2f p2(center_absolute.x + size / 2, center_absolute.y);
-    cv::Point2f p3(center_absolute.x, center_absolute.y - size / 2);
-    cv::Point2f p4(center_absolute.x, center_absolute.y + size / 2);
+    cv::Point2f p1(center_absolute.x - (float)size / 2, center_absolute.y);
+    cv::Point2f p2(center_absolute.x + (float)size / 2, center_absolute.y);
+    cv::Point2f p3(center_absolute.x, center_absolute.y - (float)size / 2);
+    cv::Point2f p4(center_absolute.x, center_absolute.y + (float)size / 2);
 
     cv::line(img, p1, p2, CV_RGB(255, 0, 0), 3);
     cv::line(img, p3, p4, CV_RGB(255, 0, 0), 3);
+}
+
+cv::Point2f App::find_object_luma(cv::Mat & frame)
+{
+    // convert to grayscale, create threshold, sum white pixels
+    // compute centroid of white pixels (average X,Y coordinate of all white pixels)
+    cv::Point2f center;
+    cv::Point2f center_normalized;
+    int tot = 0;
+
+    for (int y = 0; y < frame.rows; y++) //y
+    {
+        for (int x = 0; x < frame.cols; x++) //x
+        {
+            // load source pixel
+            cv::Vec3b pixel = frame.at<cv::Vec3b>(y, x);
+
+            // compute temp grayscale value (convert from colors to Y)
+            unsigned char Y = 0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0];
+
+            // FIND THRESHOLD (value 0..255)
+            if (Y < 240) {
+                // set output pixel black
+                frame.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 0, 0);
+            }
+            else {
+                // set output pixel white
+                frame.at<cv::Vec3b>(y, x) = cv::Vec3b(255, 255, 255);
+
+                ++tot;
+                center.x += (x - center.x) / tot;
+                center.y += (y - center.y) / tot;
+                //center_normalized.x += (x / frame.cols - center_normalized.x) / tot;
+                //center_normalized.y += (y / frame.rows - center_normalized.y) / tot;
+                center_normalized.x = center.x / frame.cols;
+                center_normalized.y = center.y / frame.rows;
+            }
+        }
+    }
+    return center_normalized;
 }
--- App.hpp
+++ App.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <opencv2/opencv.hpp>
+
 class App {
 public:
     App();
@@ -9,6 +11,7 @@ public:
 
     void draw_cross_normalized(cv::Mat& img, cv::Point2f center_relative, int size);
     void draw_cross(cv::Mat& img, int x, int y, int size);
+    cv::Point2f find_object_luma(cv::Mat & frame);
 
     ~App();
 private:
