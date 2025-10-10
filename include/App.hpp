93779aa6d024ce0d4bd92422477cb2b636bd7b1b
Libor Mahr <libormahr@gmail.com>
2025-10-10 17:47:09 +0200

    Added variables for concurrency and copied over the partial deque implementation from the materials

--- include/App.hpp
+++ include/App.hpp
@@ -3,6 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include "FpsMeter.hpp"
 #include "Config.hpp"
+#include "SyncedDequePartialImpl.hpp"
 
 class App {
 public:
@@ -19,9 +20,18 @@ public:
     ~App();
 private:
     FpsMeter fps_meter{ std::chrono::milliseconds(FPS_METER_INTERVAL)};
+
     cv::VideoCapture capture;
     cv::CascadeClassifier face_cascade;
     std::vector<cv::Point2f> find_face(cv::Mat& frame);
 
+    std::atomic<bool> tracker_terminate; //if true terminate the tracker loop
+    std::atomic<bool> tracker_buffer_empty;
+    std::atomic<std::pair<float, float>> tracker_latest_pos;
+
+    //this is just for image display in the main thread
+    //pos deque to make the crosshair synchronized with the image
+    synced_deque<std::pair<float, float>> tracker_pos_deque;
+    synced_deque<cv::Mat> tracker_frame_deque;
 };
 
